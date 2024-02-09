26ee0022cd4e5aa6ad88e7c07fe9ef5fc33b301c
Pratik-789 <pratik0400@gmail.com>
2024-02-09 21:25:49 +0530

    Ebookdetail

--- vms/src/components/UI/EbookDetail.js
+++ vms/src/components/UI/EbookDetail.js
@@ -1,15 +1,107 @@
-//EbookDetail
+// // EbookDetail
+// import React, { useState, useEffect, useRef } from 'react';
+// import { useParams, useNavigate } from 'react-router-dom';
+// import { ReactReader } from 'react-reader';
+// import { createUrl, log } from '../../utils/utils';
+// import { toast } from 'react-toastify';
+// const EbookDetail = () => {
+//   const { id } = useParams();
+//   const [ebook, setEbook] = useState(null);
+//   const readerRef = useRef();
+//   const [location, setLocation] = useState(null);
+//   const [pageNumber, setPageNumber] = useState(1); // State for page number
+//   const navigate = useNavigate();
+
+//   const locationChanged = (epubcifi) => {
+//     setLocation(epubcifi);
+//   };
+
+//   useEffect(() => {
+//   const fetchEbookDetails = async () => {
+//     try {
+//       // const url = createUrl(`/books/read/${id}`);
+//       const url = createUrl(`/books/read/2`); // Assuming 1 is the book ID
+//       const response = await fetch(url);
+//       if (response.ok) {
+//         const data = await response.json();
+//         toast.success('Success fetching ebook');
+//         const epubUrl = data.epubFileContent ? `data:application/epub+zip;base64,${data.epubFileContent}` : 'placeholder_epub_url.epub';
+//         setEbook(epubUrl);
+
+//       } else {
+//         console.error('Error fetching ebook details:', response.statusText);
+//         toast.error('Error fetching ebook details:', response.statusText);
+//       }
+//     } catch (error) {
+//       console.error('Error during fetchEbookDetails:', error);
+//       toast.error('Error during fetchEbookDetails:', error);
+//     }
+//   };
+
+//   fetchEbookDetails();
+//   console.log(ebook);
+// }, []);
+
+  
+
+//   useEffect(() => {
+//     if (readerRef.current && ebook) {
+//       readerRef.current.set('url', ebook);
+//     }
+//   }, [ebook]);
+
+//   const handleJumpToPage = () => {
+//     if (readerRef.current && pageNumber >= 1) {
+//       readerRef.current.navigate(pageNumber);
+//     }
+//   };
+
+//   if (!ebook) {
+//     return <div>Loading...</div>;
+//   }
+
+//   const goBackToHome = () => {
+//     navigate(-1);
+//   };
+
+//   return (
+//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
+//       <h2>Ebook Detail</h2>
+//       <button onClick={goBackToHome} className="btn btn-outline-dark m-2">Close</button>
+//       <div style={{ height: '100vh', width: '75vh' }}>
+//         <ReactReader
+//           location={location}
+//           title={ebook.title}
+//           locationChanged={locationChanged}
+//           url="http://localhost:8080/epub_1707450453224.epub"
+//           epubOptions={{
+//             allowPopups: true,
+//             allowScriptedContent: true,
+//           }}
+//         />
+//       </div>
+//        <div style={{ marginTop: '20px' }}>
+//         <input type="number" value={pageNumber} onChange={(e) => setPageNumber(parseInt(e.target.value))} />
+//         <button onClick={handleJumpToPage}>Jump to Page</button>
+//       </div>
+//     </div>
+    
+//   );
+// };
+// export default EbookDetail;
+
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ReactReader } from 'react-reader';
-import { createUrl, log } from '../../utils/utils';
+import { createUrl } from '../../utils/utils';
 import { toast } from 'react-toastify';
+
 const EbookDetail = () => {
   const { id } = useParams();
-  const [ebook, setEbook] = useState(null);
+  const [epubUrl, setEpubUrl] = useState(null);
   const readerRef = useRef();
   const [location, setLocation] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1); // State for page number
+  const [pageNumber, setPageNumber] = useState(1);
   const navigate = useNavigate();
 
   const locationChanged = (epubcifi) => {
@@ -19,33 +111,32 @@ const EbookDetail = () => {
   useEffect(() => {
     const fetchEbookDetails = async () => {
       try {
-        // const url = createUrl(`/books/read/${id}`);
-        const url = createUrl(`/books/read/6`);
+        const url = createUrl(`/books/read/${id}`);
         const response = await fetch(url);
         if (response.ok) {
           const data = await response.json();
           toast.success('Success fetching ebook');
-          setEbook(data);
+          const epubUrl = data.epubPath;
+          // setEpubUrl(epubUrl);
+          setEpubUrl("http://localhost:8080/epub_1707450250384.epub");
         } else {
           console.error('Error fetching ebook details:', response.statusText);
-          toast.error('Error fetching ebook');
+          toast.error('Error fetching ebook details:', response.statusText);
         }
       } catch (error) {
         console.error('Error during fetchEbookDetails:', error);
-        toast.error('Error fetching ebook');
+        toast.error('Error during fetchEbookDetails:', error);
       }
     };
 
     fetchEbookDetails();
   }, [id]);
-  console.log(ebook);
 
-  useEffect(() => {
-    if (readerRef.current && ebook) {
-      // Manually set the content of the iframe
-      readerRef.current.set('url', ebook.epub_path);
-    }
-  }, [ebook]);
+  // useEffect(() => {
+  //   if (readerRef.current && epubUrl) {
+  //     readerRef.current.set('url', epubUrl);
+  //   }
+  // }, [epubUrl]);
 
   const handleJumpToPage = () => {
     if (readerRef.current && pageNumber >= 1) {
@@ -53,47 +144,43 @@ const EbookDetail = () => {
     }
   };
 
-  if (!ebook) {
+  if (!epubUrl) {
     return <div>Loading...</div>;
   }
 
   const goBackToHome = () => {
-    // Use the navigate function to navigate back to the previous page
     navigate(-1);
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
       <h2>Ebook Detail</h2>
-      {/* <p>Ebook ID: {id}</p> */}
       <button onClick={goBackToHome} className="btn btn-outline-dark m-2">Close</button>
       <div style={{ height: '100vh', width: '75vh' }}>
         <ReactReader
           location={location}
-          title={ebook.book_name}
+          title="Ebook Title" // Pass your ebook title here
           locationChanged={locationChanged}
-          url={ebook.epub_path}
+          url={epubUrl}
           epubOptions={{
             allowPopups: true,
             allowScriptedContent: true,
           }}
+          ref={readerRef}
         />
       </div>
-      {/* <div>
-        <h3>Cover Image</h3>
-        <img
-          src={ebook.cover_image_path}
-          alt={`Cover for ${ebook.book_name}`}
-          style={{ maxWidth: '300px' }}
-        />
-      </div> */}
-       {/* <div style={{ marginTop: '20px' }}>
+      <div style={{ marginTop: '20px' }}>
         <input type="number" value={pageNumber} onChange={(e) => setPageNumber(parseInt(e.target.value))} />
         <button onClick={handleJumpToPage}>Jump to Page</button>
-      </div> */}
+      </div>
     </div>
-    
   );
 };
 
 export default EbookDetail;
+
+
+
+
+
+
